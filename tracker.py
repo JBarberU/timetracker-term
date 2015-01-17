952543e3d595866c09550f09e8af812f79de1ef5
John Barbero Unenge <john.barbero.unenge@gmail.com>
2015-01-17 18:14:14 +0100

    Progress on tracker.py

--- tracker.py
+++ tracker.py
@@ -9,28 +9,45 @@ from calendar import timegm
 class TimeObject:
 
   stop = None
+  entries = []
 
   def __init__(self, json_object=None):
     if json_object:
       self.session_start = json_object["session_start"]
-      self.start = json_object["start"]
-      self.stop = json_object["stop"] if "stop" in json_object else None
+      for jsobj in json_object["entries"]:
+        entry = {"start": jsobj["start"], "stop": jsobj["stop"] if "stop" in jsobj else None}
+        self.entries.append(entry)
     else:
       self.session_start = str(datetime.now())
-      self.start = timegm(datetime.utcnow().utctimetuple())
+      entries.append({"start": timegm(datetime.utcnow().utctimetuple()), stop: None })
 
   def stop(self):
-    self.stop = timegm(datetime.utcnow().utctimetuple())
+    for e in entries:
+      if not e.stop:
+        e.stop = timegm(datetime.utcnow().utctimetuple())
 
   def serialize(self):
     ret = {"session_start": self.session_start}
-    ret["start"] = self.start
-    if self.stop:
-      ret["stop"] = self.stop
+    entries = []
+    for e in self.entries:
+      entry = {e.start: e.start}
+      if e.stop:
+        entry["stop"] = e.stop
+
+    ret["entries"] = entries
     return ret
 
   def print_stats(self):
-    print("%s: %s" % (self.session_start, (self.stop - self.start) if self.stop else "Still running"))
+    sum_time = 0
+    running = False
+    for e in self.endries:
+      if not e.stop:
+        running = True
+      else:
+        sum_time += e.stop - e.start
+
+
+    print("%s: %s %s" % (self.session_start, sum_time, "Still running" if running else ""))
 
 class TimeTracker():
 
@@ -64,6 +81,42 @@ class TimeTracker():
   def add_timeobject(self, time_object):
     self.time_objects.append(time_object)
 
+def run(tracker):
+  keep_running = True
+
+  cmds = ["stop",
+          "pause",
+          "resume",
+          "stats",
+          "help",
+         ]
+
+  running_object = tracker.running()
+  if not running_object:
+    running_object = TimeObject()
+    tracker.add_timeobject(running_object)
+
+  while keep_running:
+
+    cmd = raw_input()
+    if cmd == cmds[0]:
+      keep_running = False
+    elif cmd == cmds[1]:
+      running_object.pause()
+    elif cmd == cmds[2]:
+      running_object.resume()
+    elif cmd == cmds[3] or cmd == "":
+      tracker.print_stats()
+    elif cmd == cmds[4]:
+      print("Available commands: \n")
+      for c in cmds:
+        print("\t{0}".format(c))
+    else:
+      print("Unrecognized command \"{0}\", try help".format(cmd))
+
+  tracker.save()
+
+
 def main():
   parser = argparse.ArgumentParser()
   parser.add_argument("-t", "--track-file", help="The file to save the \
@@ -72,14 +125,8 @@ def main():
   args = parser.parse_args()
   print args
 
-  to = TimeObject()
-  print(to.serialize())
   tracker = TimeTracker(args.track_file)
-  to.stop()
-  tracker.add_timeobject(to)
-  print(tracker.serialize())
-  tracker.print_stats()
-  tracker.save()
+  run(tracker)
 
 if __name__ == "__main__":
   main()
